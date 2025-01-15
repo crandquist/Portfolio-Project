20045f85fe47b2f6fb9a3acf5e54b315730043de
Cat Randquist <crandquist@gmail.com>
2025-01-15 14:17:46 -0800

    Add home route to Flask application

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from app import routes
-    app.register_blueprint(routes.bp)
+    @app.route('/')
+    def home():
+        return 'Welcome to my portfolio!'
 
     return app
